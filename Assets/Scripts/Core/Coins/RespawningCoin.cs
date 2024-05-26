a777c1acd716cdf806ceb4797a8d2910bfe14da1
J-Eugenio <eugeniopb4@gmail.com>
2024-05-25 22:48:06 -0300

    Coin Spawner

--- Assets/Scripts/Core/Coins/RespawningCoin.cs
+++ Assets/Scripts/Core/Coins/RespawningCoin.cs
@@ -1,9 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RespawningCoin : Coin
 {
+    public event Action<RespawningCoin> OnCollected;
+    private Vector3 previousPosition;
+
+    private void Update() {
+        if(previousPosition != transform.position){
+            Show(true);
+        }
+
+        previousPosition = transform.position;
+    }
     public override int Collect()
     {
         if(!IsServer) {
@@ -14,6 +25,14 @@ public class RespawningCoin : Coin
         if(alreadyCollected) return 0;
 
         alreadyCollected = true;
+        
+        OnCollected?.Invoke(this);
+
         return coinValue;
+    }
+
+    public void Reset()
+    {
+        alreadyCollected = false;
     }
 }
